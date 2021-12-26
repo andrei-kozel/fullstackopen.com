9db91d58828dbb6d927bc36d55030deb8bd9a1fb
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-26 10:16:21 +0100

    fixed total

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -16,7 +16,10 @@ const Content = ({ parts }) => (
   </div>
 );
 
-const Total = ({ total }) => <p>Number of exercises {total}</p>;
+const Total = ({ parts }) => {
+  const total = parts.map((part) => part.exercises).reduce((a, b) => a + b, 0);
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
   const course = {
