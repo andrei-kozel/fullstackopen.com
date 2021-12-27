7fb15cc51528ae3b8ad9801e609d9de1ce18f7fa
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-27 11:47:22 +0100

    ex2.1 completed

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, index) => (
+      <Part part={part} key={index} />
+    ))}
+  </div>
+);
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.map((part) => part.exercises).reduce((a, b) => a + b, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
