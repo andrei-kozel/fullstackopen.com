639bbd79332c65de92cd2d976862a954d7731d37
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-01-26 20:16:54 +0100

    ex3.2 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,6 +33,14 @@ app.get("/api/persons", (request, response) => {
   response.json(phonebook);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      phonebook.length
+    } people</p> <p>${new Date()}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
