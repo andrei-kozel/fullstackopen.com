160844da79fbdcbb88e4e4b5e0bb616dda090060
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-01-27 19:50:55 +0100

    ex3.13-3.14 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/phonebook.js");
 
 const app = express();
 app.use(express.static("build"));
@@ -13,86 +14,35 @@ app.use(
   )
 );
 
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  const maxId =
-    phonebook.length > 0 ? Math.max(...phonebook.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-app.get("/", (request, response) => {
-  response.send(`<h1>Phonebook app</h1>`);
-});
-
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
-});
-
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${
-      phonebook.length
-    } people</p> <p>${new Date()}</p>`
-  );
+  Person.find({}).then((result) => {
+    response.json(result);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = phonebook.find((person) => person.id === id);
-  person ? response.json(person) : response.status(404).end();
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  phonebook = phonebook.filter((person) => person.id !== id);
-  response.status(204).end();
+  Person.deleteOne({ _id: request.params.id }).then(() => {
+    response.status(204).end();
+  });
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or phone number is missing",
-    });
-  }
-
-  if (phonebook.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  phonebook.push(person);
-  response.json(person);
+  person.save().then((result) => {
+    response.json(person);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
