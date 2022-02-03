1e0a90cac892dca0300c27bc7c1b4999393f3181
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-02-03 19:17:33 +0100

    ex4.8 completed

---
+++ part4/blog-list/tests/blog_api.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: '9 HIGHLY CLICHED BUT STILL BRILLIANT BOOKS FOR BACKPACKERS',
+    author: 'Sarah Edwards',
+    url: 'https://www.natravelblog.com/9-highly-cliched-but-still-brilliant-books-for-backpackers/',
+    likes: 100
+  },
+  {
+    title: 'Test blog 2',
+    author: 'JOhn Doe',
+    url: 'https://google.com/',
+    likes: 1
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
