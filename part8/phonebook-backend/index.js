2bf4433493c96339a7135d06ce1116c8a07f0007
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-02-21 10:23:36 +0100

    mondgodb setup

--- part8/phonebook-backend/index.js
+++ part8/phonebook-backend/index.js
@@ -1,28 +1,26 @@
 const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const { v1: uuid } = require('uuid')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    phone: '040-123543',
-    street: 'Tapiolankatu 5 A',
-    city: 'Espoo',
-    id: '3d594650-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: 'Matti Luukkainen',
-    phone: '040-432342',
-    street: 'Malminkaari 10 A',
-    city: 'Helsinki',
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: 'Venla Ruuska',
-    street: 'Nallemäentie 22 C',
-    city: 'Helsinki',
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  }
-]
+const MONGO_DB_URL =
+  'mongodb+srv://husky_dusky:3134319Bexa@cluster0.5edqh.mongodb.net/phonebook-gql?retryWrites=true&w=majority'
+
+console.log('connecting to ', MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const typeDefs = gql`
   enum YesNo {
@@ -61,16 +59,17 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    personCount: async () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
-      if (!args.phone) {
-        return persons
-      }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
+      // if (!args.phone) {
+      //   return persons
+      // }
+      // const byPhone = (person) =>
+      //   args.phone === 'YES' ? person.phone : !person.phone
+      // return persons.filter(byPhone)
+      return Person.find({})
     },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name)
+    findPerson: async (root, args) => Person.findOne({ name: args.name })
   },
   Person: {
     address: (root) => {
@@ -87,19 +86,13 @@ const resolvers = {
           invalidArgs: args.name
         })
       }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
-      return person
+      const person = new Person({ ...args })
+      return person.save()
     },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name)
-      if (!person) {
-        return null
-      }
-
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
-      return updatedPerson
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      return person.save()
     }
   }
 }
